bc51808a69284c7f166ffc40155ae22f85754874
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-05-04 11:34:49 -0400

    learn: error handler in setSide square class

--- bin/getters_and_setters.dart
+++ bin/getters_and_setters.dart
@@ -1,15 +1,19 @@
 void main() {
   final square = Square(side: 5);
   print('Area of the square: ${square.area}'); // Output: Area of the square: 25.0
-  square.setSide = 10;
+  square.setSide = -10;
   print('New area of the square: ${square.area}'); // Output: New area of the square: 100.0
 }
 
 class Square {
-  double side = 0;
+  double side;
 
   Square({required this.side});
 
   double get area => side * side;  
-  set setSide(double newSide) => side = newSide;
+  set setSide(double newSide) => {
+    if(newSide < 0) throw ArgumentError('Side length cannot be negative'),
+
+    side = newSide
+    };
 }
