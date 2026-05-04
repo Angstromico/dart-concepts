f2828a0bb223a2bd3512906102fc01b88dd5709a
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-05-04 16:24:51 -0400

    learn: yield emitPerodicNUmbers

--- bin/streams.dart
+++ bin/streams.dart
@@ -1,11 +1,17 @@
 void main() {
   final numberStream = emitNumbers(5);
+  final periodicStream = emitPeriodicNumbers(5, Duration(seconds: 1));
 
   numberStream.listen(
     (number) => print('Received: $number'),
     onDone: () => print('Stream is done!'),
     onError: (error) => print('Error: $error'),
   );
+  periodicStream.listen(
+    (number) => print('Periodic Received: $number'),
+    onDone: () => print('Periodic Stream is done!'),
+    onError: (error) => print('Periodic Error: $error'),
+  ); //This only shows "Periodic Stream is done!" that is good?
 }
 
 Stream<int> emitNumbers(int n) async* {
@@ -13,4 +19,8 @@ Stream<int> emitNumbers(int n) async* {
     await Future.delayed(Duration(seconds: 1)); // Simulate some delay
     yield i; // Emit the next number in the stream
   }
+}
+
+Stream<int> emitPeriodicNumbers(int n, Duration interval) async* {
+  yield* Stream.castFrom(  Stream.periodic(interval, (count) => count).take(n));
 }
